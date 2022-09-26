304668333bfe718565771025770824783a22def7
Austine1996 <austinenyiam12@gmail.com>
2022-09-26 21:10:48 +0100

    import_modules git commit -m import_modules git commit -m import_modules

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    digit = 0
     if number < 0:
-        digit = -number % 10
-        print(digit, end="")
+        number = number * -1
+        print(number % 10, end="")
+        return number % 10
     else:
-        digit = number % 10
-        print(digit, end="")
-    return digit
+        print(number % 10, end="")
+        return number % 10
